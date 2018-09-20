e3fe08f602255ee83544267c68b44d05e3b0813f
ma hongb <your_email@youremail.com>
2018-09-20 23:45:08 +0800

    进行首页生成 2018.09.20 mahongb

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>123456</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
